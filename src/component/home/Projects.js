5f1ad1db0df416188853ef9942a027b3f77a9044
admin <anasszeroual09@gmail.com>
2023-12-03 15:34:54 +0100

    g

--- src/component/home/Projects.js
+++ src/component/home/Projects.js
@@ -1,6 +1,12 @@
-
+import c from "./Projects.module.css";
 
 const Projects=p=>{
+
+    return(
+        <div className={c.projectsContainer} >
+            <h3>Projects</h3>
+        </div>
+    )
 
 }
 export default Projects;
