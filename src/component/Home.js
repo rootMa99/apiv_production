547d8c585bd919bb1b63308f77b369d767b435ff
admin <anasszeroual09@gmail.com>
2023-12-04 08:20:21 +0100

    tu

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,12 +1,13 @@
 import c from "./Home.module.css";
 import Efficiency from "./home/Efficiency";
+import Projects from "./home/Projects";
 
 const Home = (p) => {
   return (
     <div className={c.container}>
       <div className={c.content}>
         <Efficiency />
-        
+        <Projects />
       </div>
     </div>
   );
--- src/component/home/Projects.js
+++ src/component/home/Projects.js
@@ -5,6 +5,9 @@ const Projects=p=>{
     return(
         <div className={c.projectsContainer} >
             <h3>Projects</h3>
+            <p>
+                lkjbkkjnksnffknsdkjnnqndv kdqsjlkf;q,snd;fqnsfljklq 
+            </p>
         </div>
     )
 
