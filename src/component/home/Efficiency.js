5a9f7ce500fe47879e835af03a3e9ee4e566eb43
admin <anasszeroual09@gmail.com>
2023-12-05 14:19:13 +0100

    uy

--- src/component/home/Efficiency.js
+++ src/component/home/Efficiency.js
@@ -1,22 +1,21 @@
+import React from "react";
 import c from "./Efficiency.module.css";
 import EfficiencyData from "./EfficiencyData";
 
-
-const Efficiency=p=>{
-
-
-    return(
-        <div className={c.efficiency}>
-          <h3>efficiency</h3>
-          <div className={c.efficiencyContent}>
-            <EfficiencyData title="last day" />      
-            <EfficiencyData title="month" />  
-            <EfficiencyData title="year" />      
-            <EfficiencyData title="hc/day" />      
-
-          </div>
+const Efficiency = (p) => {
+  return (
+    <React.Fragment>
+      <h3 className={c.title}>efficiency</h3>
+      <div className={c.efficiency}>
+        <div className={c.efficiencyContent}>
+          <EfficiencyData title="last day" />
+          <EfficiencyData title="month" />
+          <EfficiencyData title="year" />
+          <EfficiencyData title="hc/day" />
         </div>
-    )
-}
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Efficiency;
--- src/component/home/EfficiencyData.js
+++ src/component/home/EfficiencyData.js
@@ -1,21 +1,31 @@
+import React from "react";
 import c from "./EfficiencyData.module.css";
 
 const EfficiencyData = (p) => {
   return (
     <div className={c.efficiencyData}>
-      <h4>{p.title} </h4>
+      <h4 className={c.title}>{p.title} </h4>
       <div className={c.data}>
         <div className={c.total}>
-          <h5>total:</h5>
-          <span>87</span>
+          {p.title === "hc/day" ? (
+            <React.Fragment>
+              <h5 className={c.title}>total</h5>
+              <span>67</span>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <h4 className={c.title}>70%</h4>
+              <progress value="70" max="100"></progress>
+            </React.Fragment>
+          )}
         </div>
         <div className={c.addData}>
           <div className={c.gap}>
-            <h5>gap:</h5>
+            <h5 className={c.title}>gap:</h5>
             <span>67</span>
           </div>
           <div className={c.target}>
-            <h5>target:</h5>
+            <h5 className={c.title}>target:</h5>
             <span>67</span>
           </div>
         </div>
