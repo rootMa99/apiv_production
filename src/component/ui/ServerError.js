f06787cdcc3ce822401049138b9a4b5604419f85
admin <anasszeroual09@gmail.com>
2024-01-02 16:44:56 +0100

    hut

--- src/component/ui/ServerError.js
+++ src/component/ui/ServerError.js
@@ -1,9 +1,15 @@
 import c from "./ServerError.module.css";
 import logo from "../../assets/500Server.png";
+import logoor from "../../assets/aptiv_logo_rev_orange_rgb.png";
+
 
 const ServerError = (p) => {
   return (
     <div className={c.wrapper}>
+    <div className={c.par}>
+        <img src={logoor} alt="logo aptiv" className={c.imgs} />
+        <p className={c.message}>We encountered an error from the server</p>
+    </div>
       <img src={logo} alt="no connection" className={c.img} />
     </div>
   );
