ee36c96fb8946fc189c20e43f4e1e8e2735407d7
admin <anasszeroual09@gmail.com>
2024-01-05 08:38:31 +0100

    tar

--- src/component/home/OtherProjects.js
+++ src/component/home/OtherProjects.js
@@ -1,20 +1,23 @@
-import { getFiltredProjectOther } from "../hooks/getEfficiencyData";
 import ProjectCard from "./ProjectCard";
 import c from "./OtherProjects.module.css";
 
-
-const OtherProjects=p=>{
-
-
-console.log(p.sortedData)
-return(
-    <div className={c.container} >
-        {p.sortedData.map((m) => (
-            <ProjectCard key={m.name} title={m.name} data={m.data}  day={p.day} month={p.month} pic={m.projectUriPic} other={true} />
-          ))}
+const OtherProjects = (p) => {
+  console.log(p.sortedData);
+  return (
+    <div className={c.container}>
+      {p.sortedData.map((m) => (
+        <ProjectCard
+          key={m.name}
+          title={m.name}
+          data={m.data}
+          day={p.day}
+          month={p.month}
+          pic={m.projectUriPic}
+          other={true}
+        />
+      ))}
     </div>
-)
-
-}
+  );
+};
 
 export default OtherProjects;
