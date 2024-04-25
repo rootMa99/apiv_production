8f71349512a2f0f99a0a8d32c89037f29f07c342
anass zeroual <anasszeroual09@gmail.com>
2024-04-25 16:39:35 +0100

    ttre

--- src/component/comparison/Comparison.js
+++ src/component/comparison/Comparison.js
@@ -1,16 +1,44 @@
 import { useSelector } from "react-redux";
 import { destractData} from "../hooks/newDataManpulate";
 import TlAndCrew from "./TlAndCrew";
-
+import { useState } from "react";
+import c from "./TlAndCrew.module.css"
+import Teamleader from "./Teamleader";
 
 const Comparison = (p) => {
   let data = useSelector((s) => s.datas);
   const { date } = useSelector((s) => s.additionalData);
   const fd = destractData(data).filter((f) => f.date === date && f.teamLeader!==null);
- 
+    const [control, setControl]=useState("tlc")
+
+
   return (
     <div style={{ width: "100%"}}>
-        <TlAndCrew fd={fd} />
+    <ul className={c.underList}>
+    <li
+      style={
+        control === "tlc"
+          ? { opacity: 1, borderBottom: "2px solid white" }
+          : {}
+      }
+    onClick={e=>setControl("tlc")}
+    >
+      teamLeader by crew
+    </li>
+
+    <li
+      style={
+        control === "tl"
+          ? { opacity: 1, borderBottom: "2px solid white" }
+          : {}
+      }
+      onClick={e=>setControl("tl")}
+    >
+      Teamleader
+    </li>
+  </ul>
+        {control==="tlc"&&<TlAndCrew fd={fd} />}
+        {control==="tl"&&<Teamleader fd={fd} />}
     </div>
   )
 };
