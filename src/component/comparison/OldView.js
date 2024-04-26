a8efa423da680fc4662ab9f9851e55dbfe9e01c9
anass zeroual <anasszeroual09@gmail.com>
2024-04-26 10:38:54 +0100

    salop

--- src/component/comparison/OldView.js
+++ src/component/comparison/OldView.js
@@ -3,17 +3,25 @@ import c from "./OldView.module.css";
 const OldView = (p) => {
   return (
     <div className={c.container}>
-      <span className={c.ngap}>-52</span>
+      {p.data.gap < 0 && (
+        <span className={c.ngap}>{p.data.gap.toFixed(1)}</span>
+      )}
       <div className={c.mainChart}>
         <div className={c.target}>
-          <div className={c.colrize} style={{width:"86%"}}>86%</div>
+          <div className={c.colrize} style={{ width: "86%" }}>
+            {p.data.effTar.toFixed(1)}
+          </div>
         </div>
-        <div className={c.nameContainer}> BROUROU MOHAMED </div>
+        <div className={c.nameContainer}> {p.data.name} </div>
         <div className={c.actual}>
-          <div className={c.colrize} style={{width:"90%"}}>90%</div>
+          <div className={c.colrize} style={{ width: "90%" }}>
+            {p.data.eff.toFixed(1)}
+          </div>
         </div>
       </div>
-      <span className={c.pgap}>+9</span>
+      {p.data.gap > 0 && (
+        <span className={c.pgap}>{p.data.gap.toFixed(1)}</span>
+      )}
     </div>
   );
 };
