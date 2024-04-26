a419f6eac8de1f02158e11d472f146bf8a9a9be6
anass zeroual <anasszeroual09@gmail.com>
2024-04-26 10:04:19 +0100

    trz

--- src/component/comparison/OldView.js
+++ src/component/comparison/OldView.js
@@ -6,11 +6,11 @@ const OldView = (p) => {
       <span className={c.ngap}>-52</span>
       <div className={c.mainChart}>
         <div className={c.target}>
-          <div className={c.colrize}>86%</div>
+          <div className={c.colrize} style={{width:"86%"}}>86%</div>
         </div>
         <div className={c.nameContainer}> BROUROU MOHAMED </div>
         <div className={c.actual}>
-          <div className={c.colrize}>90%</div>
+          <div className={c.colrize} style={{width:"90%"}}>90%</div>
         </div>
       </div>
       <span className={c.pgap}>+9</span>
