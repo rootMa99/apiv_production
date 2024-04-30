2ceee24ed95c4dcac740039825968f9cadfeabb7
anass zeroual <anasszeroual09@gmail.com>
2024-04-30 15:25:22 +0100

    wsdr

--- src/component/comparison/OldView.js
+++ src/component/comparison/OldView.js
@@ -1,27 +1,46 @@
 import c from "./OldView.module.css";
 
 const OldView = (p) => {
-  console.log(p.type, p.data[p.type])
+  console.log(p.type, p.data[p.type]);
   return (
     <div className={c.container}>
       {p.data[p.type] < 0 && (
-        <span className={c.ngap} style={p.act==="abs"?{color:"green"}:{}} >{p.data[p.type].toFixed(1)}</span>
+        <span
+          className={c.ngap}
+          style={p.act === "abs" ? { color: "green" } : {}}
+        >
+          {p.data[p.type].toFixed(1)}
+        </span>
       )}
       <div className={c.mainChart}>
-        {p.tar!==0&&<div className={c.target}>
-          <div className={c.colrize} style={{ width: `${p.data[p.tar]}%`}}>
-            {p.data[p.tar].toFixed(1)}
+        {p.tar !== 0 && (
+          <div className={c.target}>
+            <div className={c.colrize} style={{ width: `${p.data[p.tar]}%` }}>
+              {p.data[p.tar].toFixed(1)}
+            </div>
           </div>
-        </div>}
+        )}
         <div className={c.nameContainer}> {p.data.name} </div>
         <div className={c.actual}>
-          <div className={c.colrize} style={{ width: `${p.data[p.act]}%`}}>
+          <div
+            className={c.colrize}
+            style={
+              p.data[p.type] > 0 || p.data[p.type] === undefined
+                ? { width: `${p.data[p.act]}%` }
+                : { width: `${p.data[p.act]}%`, backgroundColor: "#CF3335" }
+            }
+          >
             {p.data[p.act].toFixed(1)}
           </div>
         </div>
       </div>
       {p.data[p.type] > 0 && (
-        <span className={c.pgap} style={p.act==="abs"?{color:"red"}:{}} >{p.data[p.type].toFixed(1)}</span>
+        <span
+          className={c.pgap}
+          style={p.act === "abs" ? { color: "red" } : {}}
+        >
+          {p.data[p.type].toFixed(1)}
+        </span>
       )}
     </div>
   );
